32f3cabfb8e53e1a987d0965fc8a0437e86241a1
XDeme <fernando.tagawa.gamail.com@gmail.com>
2023-10-14 14:39:35 -0300

    [algorithm] Added personalizable min and max

--- include/xme/algorithm/min_max.hpp
+++ include/xme/algorithm/min_max.hpp
@@ -9,17 +9,38 @@ constexpr auto min(const T& lhs, const T& rhs) noexcept -> const T& {
     return rhs;
 }
 
-template<typename T>
-constexpr auto max(const T& lhs, const T& rhs) noexcept -> const T& {
-    if (lhs > rhs)
+template<typename T, typename Cmp>
+constexpr auto min(const T& lhs, const T& rhs, Cmp cmp) noexcept -> const T& {
+    if (cmp(lhs, rhs))
         return lhs;
     return rhs;
 }
 
 template<typename T>
-constexpr auto minmax(const T& lhs, const T& rhs) noexcept
-    -> const Pair<const T&, const T&> {
+constexpr auto max(const T& lhs, const T& rhs) noexcept -> const T& {
     if (lhs < rhs)
+        return rhs;
+    return lhs;
+}
+
+template<typename T, typename Cmp>
+constexpr auto max(const T& lhs, const T& rhs, Cmp cmp) noexcept -> const T& {
+    if (cmp(lhs, rhs))
+        return rhs;
+    return lhs;
+}
+
+template<typename T>
+constexpr auto minmax(const T& lhs, const T& rhs) noexcept -> Pair<const T&, const T&> {
+    if (lhs < rhs)
+        return {lhs, rhs};
+    return {rhs, lhs};
+}
+
+template<typename T, typename Cmp>
+constexpr auto minmax(const T& lhs, const T& rhs, Cmp cmp) noexcept
+    -> Pair<const T&, const T&> {
+    if (cmp(lhs, rhs))
         return {lhs, rhs};
     return {rhs, lhs};
 }
