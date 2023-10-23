d5d29cbe46a63b969a838e962bb74f745f407061
XDeme <fernando.tagawa.gamail.com@gmail.com>
2023-10-22 22:35:43 -0300

    Transfered the rest of matrix test to a sigular test

--- tests/math/matrices/common.hpp
+++
@@ -1,6 +0,0 @@
-#include <xme/math/math.hpp>
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-constexpr bool isError(bool b) { return b == false; }
--- tests/math/matrix.cpp
+++ tests/math/matrix.cpp
@@ -83,6 +83,118 @@ int test_operators() {
             std::cerr << "xme::Matrix / operator error\n";
         }
     }
+    {
+        math::mat3 m{5};
+        static_assert(std::is_same_v<decltype(-m), math::mat3>);
+
+        bool error = -m != math::mat3{-5};
+        if (error) {
+            std::cerr << "xme::Matrix unary operator error\n";
+            ++errors;
+        }
+    }
+    return errors;
+}
+
+int test_self_operators() {
+    int errors = 0;
+    {
+        math::mat3 m;
+        m += 3;
+
+        bool error = m[0] != math::vec3{4, 3, 3} || m[1] != math::vec3{3, 4, 3};
+        error |= m[2] != math::vec3{3, 3, 4};
+
+        m += math::mat3{3};
+
+        error |= m[0] != math::vec3{7, 3, 3} || m[1] != math::vec3{3, 7, 3};
+        error |= m[2] != math::vec3{3, 3, 7};
+        if (error) {
+            std::cerr << "xme::Matrix += operator error\n";
+            ++errors;
+        }
+    }
+    {
+        math::mat3 m;
+        m -= 3;
+
+        bool error = m[0] != math::vec3{-2, -3, -3} || m[1] != math::vec3{-3, -2, -3};
+        error |= m[2] != math::vec3{-3, -3, -2};
+
+        m -= math::mat3{3};
+
+        error |= m[0] != math::vec3{-5, -3, -3} || m[1] != math::vec3{-3, -5, -3};
+        error |= m[2] != math::vec3{-3, -3, -5};
+        if (error) {
+            std::cerr << "xme::Matrix -= operator error\n";
+            ++errors;
+        }
+    }
+    {
+        math::mat3 m;
+        m *= 3;
+
+        bool error = m[0] != math::vec3{3, 0, 0} || m[1] != math::vec3{0, 3, 0};
+        error |= m[2] != math::vec3{0, 0, 3};
+        if (error) {
+            std::cerr << "xme::Matrix *= operator error\n";
+            ++errors;
+        }
+    }
+    {
+        math::mat2 m;
+        m /= 2;
+
+        bool error = m[0] != math::vec2{0.5, 0} || m[1] != math::vec2{0, 0.5};
+        if (error) {
+            ++errors;
+            std::cerr << "xme::Matrix /= operator error\n";
+        }
+    }
+    return errors;
+}
+
+int test_matrix_multiplication() {
+    int errors = 0;
+    static_assert(std::is_same_v<decltype(math::mat2{} * math::vec2{}), math::vec2>);
+    static_assert(std::is_same_v<decltype(math::mat3{} * math::vec3{}), math::vec3>);
+    static_assert(std::is_same_v<decltype(math::mat4{} * math::vec4{}), math::vec4>);
+    static_assert(std::is_same_v<decltype(math::Matrix<float, 4, 1>{} * math::vec4{}),
+                                 math::Vector<float, 1>>);
+    static_assert(
+        std::is_same_v<decltype(math::Matrix<float, 3, 2>{} * math::vec3{}), math::vec2>);
+    {
+        math::mat4 m{2};
+        auto v = m * math::vec4{5, 6, 7, 1};
+
+        bool error = v != math::vec4{10, 12, 14, 2};
+        if (error) {
+            std::cerr << "xme::Matrix multiplication 1 Error\n";
+            ++errors;
+        }
+    }
+    {
+        math::Matrix<float, 4, 2> m{math::vec2{1, 2}, math::vec2{9, 9}, math::vec2{1},
+                                    math::vec2{5}};
+        auto v = m * math::vec4{2};
+
+        bool error = v != math::vec2{32, 34};
+        if (error) {
+            std::cerr << "xme::Matrix multiplication 2 error\n";
+            ++errors;
+        }
+    }
+
+    {
+        math::mat2 m{math::vec2{1, 3}, math::vec2{7, 4}};
+        auto r = m * m;
+
+        bool error = r[0] != math::vec2{22, 15} || r[1] != math::vec2{35, 37};
+        if (error) {
+            std::cerr << "xme::Matrix multiplication 3 Error";
+            ++errors;
+        }
+    }
     return errors;
 }
 
@@ -121,6 +233,8 @@ int main() {
     int errors = 0;
     errors += test_access();
     errors += test_operators();
+    errors += test_self_operators();
+    errors += test_matrix_multiplication();
     errors += test_basic_functions();
     return errors;
 }
