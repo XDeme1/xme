a41e940ac875b15783f41ba2e59cfd35d4febb3e
XDeme <fernando.tagawa.gamail.com@gmail.com>
2023-12-08 14:00:15 -0300

    Fix const correctness

--- include/xme/ranges/destroy.hpp
+++ include/xme/ranges/destroy.hpp
@@ -6,7 +6,7 @@ namespace xme::ranges {
 namespace detail {
 struct DestroyA {
     template<std::forward_iterator Iter, std::sentinel_for<Iter> Sent, typename Alloc>
-    constexpr void operator()(Iter first, Sent last, Alloc& alloc) noexcept {
+    constexpr void operator()(Iter first, Sent last, Alloc& alloc) const noexcept {
         using traits = std::allocator_traits<Alloc>;
         for(; first != last; ++first) {
             traits::destroy(alloc, std::addressof(*first));
@@ -20,7 +20,7 @@ inline constexpr detail::DestroyA destroy_a;
 namespace detail {
 struct DestroyAtA {
     template<std::destructible T, typename Alloc>
-    constexpr void operator()(T* pos, Alloc& alloc) noexcept {
+    constexpr void operator()(T* pos, Alloc& alloc) const noexcept {
         using traits = std::allocator_traits<Alloc>;
         if constexpr(std::is_array_v<T>) {
             ranges::destroy_a(std::ranges::begin(*pos), std::ranges::end(*pos), alloc);
@@ -37,7 +37,8 @@ inline constexpr detail::DestroyAtA destroy_at_a;
 namespace detail {
 struct DestroyNA {
     template<std::forward_iterator Iter, typename Alloc>
-    constexpr void operator()(Iter first, std::iter_difference_t<Iter> n, Alloc& alloc) noexcept {
+    constexpr void
+    operator()(Iter first, std::iter_difference_t<Iter> n, Alloc& alloc) const noexcept {
         for(; n > 0; ++first, (void)--n) {
             ranges::destroy_at_a(std::addressof(*first), alloc);
         }
