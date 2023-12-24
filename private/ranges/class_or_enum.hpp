caf2828f402519ca63a866ceda35050417c5de56
XDeme <fernando.tagawa.gamail.com@gmail.com>
2023-12-23 22:22:13 -0300

    Added ranges::swap

---
+++ private/ranges/class_or_enum.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <type_traits>
+
+namespace xme::ranges::detail {
+template<typename T>
+concept class_or_enum = std::is_class_v<T> || std::is_union_v<T> || std::is_enum_v<T>;
+}
---
+++ tests/ranges/swap.cpp
@@ -0,0 +1,40 @@
+#include <cassert>
+#include <xme/ranges/swap.hpp>
+#include <xme/container/tuple.hpp>
+
+int main() {
+    {
+        int a = 1;
+        int b = 2;
+        static_assert(noexcept(xme::ranges::swap(a, b)));
+        xme::ranges::swap(a, b);
+        assert(a == 2 && b == 1);
+    }
+
+    {
+        xme::Tuple<int> a{5};
+        xme::Tuple<int> b{3};
+        static_assert(noexcept(xme::ranges::swap(a, b)));
+        xme::ranges::swap(a, b);
+        assert(get<0>(a) == 3 && get<0>(b) == 5);
+    }
+
+    {
+        struct Foo {
+            int a = 0;
+        };
+        Foo a{2};
+        Foo b{7};
+        static_assert(noexcept(xme::ranges::swap(a, b)));
+        xme::ranges::swap(a, b);
+        assert(a.a == 7 && b.a == 2);
+    }
+
+    {
+        std::array<int, 2> a{5, 3};
+        std::array<int, 2> b{1, 2};
+        static_assert(noexcept(xme::ranges::swap(a, b)));
+        xme::ranges::swap(a, b);
+        assert(a[0] == 1 && a[1] == 2 && b[0] == 5 && b[1] == 3);
+    }
+}
