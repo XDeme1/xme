802d578e1cf3d0dc1843e4a9729ee09dcb56988d
XDeme <fernando.tagawa.gamail.com@gmail.com>
2024-04-19 12:58:20 -0300

    Add IContainer

---
+++ tests/container/icontainer.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <xme/container/icontainer.hpp>
+#include <gtest/gtest.h>
+
+struct Container : public xme::IContainer<Container> {
+    constexpr auto begin() noexcept { return v.begin(); }
+    constexpr auto begin() const noexcept { return v.begin(); }
+
+    constexpr auto end() noexcept { return v.end(); }
+    constexpr auto end() const noexcept { return v.end(); }
+
+    std::vector<int> v;
+};
+
+struct IContainerTest : ::testing::Test {
+    IContainerTest() {
+        for(auto&& v : {5, 3, 5, 1}) {
+            c.v.emplace_back(v);
+        }
+    }
+
+    Container c;
+};
+
+TEST_F(IContainerTest, Range) {
+    auto b = c.begin();
+    EXPECT_EQ(*b++, 5);
+    EXPECT_EQ(*b++, 3);
+    EXPECT_EQ(*b++, 5);
+    EXPECT_EQ(*b++, 1);
+    EXPECT_EQ(b, c.end());
+}
+
+TEST_F(IContainerTest, Methods) {
+    EXPECT_FALSE(c.empty());
+    EXPECT_EQ(c.size(), 4);
+    EXPECT_EQ(c.front(), 5);
+    EXPECT_EQ(c.back(), 1);
+}
